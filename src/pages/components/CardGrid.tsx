07e0310c05e4524dae4554a25e26dafb6418a533
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2024-10-30 19:15:08 -0400

    ranking

--- src/pages/components/CardGrid.tsx
+++ src/pages/components/CardGrid.tsx
@@ -1,21 +1,22 @@
 // CardGrid.tsx
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import { Card, CardMedia, CardContent, Typography } from '@mui/material';
+import { Card, CardMedia } from '@mui/material';
 
-const CardGrid: React.FC<{ cards: { name: string; imageUrl: string }[] }> = ({ cards }) => {
+const CardGrid: React.FC<{ cards: { name: string; imageUrl: string, rank: number }[] }> = ({ cards }) => {
 
-  
   return (
-    <Grid container spacing={2} style={{ marginTop: 15, justifyContent: 'center'}}>
-      {cards.map((card) => (
+    <Grid container spacing={2} style={{ marginTop: 15, justifyContent: 'center' }}>
+    {cards
+      .sort((a, b) => a.rank - b.rank) // Sort the cards by rank
+      .map((card) => (
         <Grid item xs={2} key={card.name}>
           <Card>
             <CardMedia component="img" image={card.imageUrl} alt={card.name} />
           </Card>
         </Grid>
       ))}
-    </Grid>
+  </Grid>
   );
 };
 
