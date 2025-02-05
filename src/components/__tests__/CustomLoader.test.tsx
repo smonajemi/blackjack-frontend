ffecf506bf3945ca1990cfb9d2b716f5db80a01d
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2025-02-05 12:35:25 -0500

    Update CustomLoader.test.tsx

--- src/components/__tests__/CustomLoader.test.tsx
+++ src/components/__tests__/CustomLoader.test.tsx
@@ -6,7 +6,6 @@ import CustomLoader from '..../CustomLoader
 describe('CustomLoader', () => {
   it('renders the spinner', () => {
     render(<CustomLoader />);
-    // Check if CircularProgress is in the document
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
   });
 
@@ -14,7 +13,6 @@ describe('CustomLoader', () => {
     const message = 'Loading...';
     render(<CustomLoader message={message} />);
 
-    // Check if the message is displayed
     expect(screen.getByText(message)).toBeInTheDocument();
   });
 });
