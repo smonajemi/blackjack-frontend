610a7d18dbfd567f1bfe512196208ce107edb7e2
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2025-02-05 12:37:13 -0500

    Update Dashboard.test.tsx

--- src/pages/components/__tests__/Dashboard.test.tsx
+++ src/pages/components/__tests__/Dashboard.test.tsx
@@ -1,14 +1,13 @@
-// Dashboard.test.tsx
 import React from 'react';
 import { render } from '@testing-library/react';
-import Dashboard from '../../Dashboard'; // Adjust the import path if needed
+import Dashboard from '../../Dashboard'; 
 import '@testing-library/jest-dom';
 
 
 describe('Dashboard', () => {
   test('renders without crashing', () => {
     render(<Dashboard />);
-    // You can add a simple assertion here if needed, like checking for an element
-    expect(true).toBe(true); // This is a placeholder assertion
+   
+    expect(true).toBe(true);
   });
 });
